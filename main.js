fd2af32fbdcad0a73e92728750439a00822dc9da
kelliaUmuhire <kellumuhire@gmail.com>
2024-05-17 19:31:41 +0200

    Sticky navbar

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+let nav = document.querySelector("nav");
+let sticky = nav.offsetTop;
+
 const toggleMenu = () => {
   document.getElementById("menuLinks").classList.toggle("hidden");
   document.getElementById("menuLinks").classList.toggle("flex");
 };
+
+const checkPosition = () => {
+  if (window.scrollY >= sticky) {
+    nav.classList.add("fixed_navbar");
+  } else {
+    nav.classList.remove("fixed_navbar");
+  }
+};
+
+window.addEventListener("scroll", checkPosition);
